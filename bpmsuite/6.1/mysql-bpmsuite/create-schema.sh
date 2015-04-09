96a19f2c3b54e79fa651bf994c8f22c319f822e1
jbride <jeff bride>
2015-04-09 05:52:38 -0600

    latest

--- bpmsuite/6.1/mysql-bpmsuite/create-schema.sh
+++ bpmsuite/6.1/mysql-bpmsuite/create-schema.sh
@@ -5,5 +5,12 @@ mysql -u root -e "GRANT ALL ON *.* TO 'jbpm'@'localhost' IDENTIFIED BY 'jbpm';"
 mysql -u root -e "GRANT ALL ON *.* TO 'jbpm'@'%' IDENTIFIED BY 'jbpm';"
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS jbpm"
 
+mysql -u root -e "GRANT ALL ON *.* TO 'dashbuilder'@'localhost' IDENTIFIED BY 'dashbuilder';"
+mysql -u root -e "GRANT ALL ON *.* TO 'dashbuilder'@'%' IDENTIFIED BY 'dashbuilder';"
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS dashbuilder"
+
 mysql -u root jbpm < /sql/mysql5-jbpm-schema.sql
 mysql -u root jbpm < /sql/quartz_tables_mysql.sql
+
+# TO-DO:  use dashbuilder database
+mysql -u root jbpm < /sql/mysql5-dashbuilder-schema.sql
